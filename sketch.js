23617fdd325fa56c346b1316f494df4ced88c612
Krishank696 <73972335+Krishank696@users.noreply.github.com>
2021-03-24 13:39:50 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var hero;
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+function mouseDragged(){
+  Matter.body.setPosition(hero.body, {x: mouseX, y: mouseY});
+}
